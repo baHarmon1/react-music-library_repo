73caaab3e2bb2c0e21476ee0fcf48ae1868e1b62
Brycen Harmon <brycenharmon@gmail.com>
2021-11-18 10:21:31 -0600

    added three components to satisfy user stories, will add functionality at a later time

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,37 +1,54 @@
 import axios from "axios";
 import React, { Component } from "react";
+import CreateSong from "./CreateSong/CreateSong";
 import MusicLibraryTable from "./MusicLibraryTable/MusicLibraryTable";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      table: []
+      table: [],
     };
   }
 
   componentDidMount() {
     this.getTable();
-}
-
-async getTable() {
-    try{
-        let responseTable = await axios.get("http://127.0.0.1:8000/music/");
-          this.setState({
-                  table: responseTable.data
-              });
-            console.log(responseTable);
-      }
-      catch (ex) {
-          console.log('Error in API call!')
-      }
-
   }
 
+  async getTable() {
+    try {
+      let responseTable = await axios.get("http://127.0.0.1:8000/music/");
+      this.setState({
+        table: responseTable.data,
+      });
+      console.log(responseTable);
+    } catch (ex) {
+      console.log("Error in API call!");
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  deleteRow(event) {
+    try {
+      await axios.delete(event.target);
+    } catch (ex) {
+      console.log("Error in API call!");
+    }
+  }
   render() {
     return (
       <div>
-        <MusicLibraryTable table={this.state.table}/>
+        <MusicLibraryTable table={this.state.table} />
+        <CreateSong />
         <h1>Test from App</h1>
       </div>
     );
